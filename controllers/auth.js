d5b6629c33a77ebb00a52b1da4b10e02fab740dc
pratik-das <pratik16082001@gmail.com>
2023-06-30 05:03:53 +0530

    validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,18 @@
+const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const nodemailer = require("nodemailer");
+const { validationResult } = require("express-validator");
+
+//directly using nodemailer to send mails
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "pratik16082001@gmail.com",
+    //password generated via two-step authentication
+    pass: "yefdcajaopkerldp",
+  },
+});
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -62,7 +75,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
@@ -82,6 +103,15 @@ exports.postSignup = (req, res, next) => {
         .then((result) => {
           console.log("SIGNUP COMPLETE!");
           res.redirect("/login");
+          return transporter.sendMail({
+            to: email,
+            from: "pratik16082001@gmail.com",
+            subject: "Signup succeeded!",
+            html: "<h1>You successfully signed up!</h1>",
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
     })
     .catch((err) => {
@@ -94,4 +124,108 @@ exports.postLogout = (req, res, next) => {
     console.log(err);
     res.redirect("/");
   });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) message = message[0];
+  else message = null;
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "No account with that email found.");
+          return res.redirect("/reset");
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user
+          .save()
+          .then((result) => {
+            res.redirect("/login");
+            transporter.sendMail({
+              to: req.body.email,
+              from: "pratik16082001@gmail.com",
+              subject: "Password Reset!",
+              html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password</p>`,
+            });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({
+    resetToken: token,
+    resetTokenExpiration: {
+      $gt: Date.now(),
+      //gt->greater than
+    },
+  })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) message = message[0];
+      else message = null;
+      res.render("auth/new-password", {
+        path: "/new-password",
+        pageTitle: "New Password",
+        errorMessage: message,
+        passwordToken: token,
+        userId: user._id.toString(),
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,44 @@
-const express = require('express');
+const express = require("express");
+//destructuring
+const { check, body } = require("express-validator");
 
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post(
+  "/signup",
+  check("email").isEmail().withMessage("Please enter a valid e-mail."),
+  body(
+    "password",
+    "Your password should be alphanumeric and at least 5 characters long."
+  )
+    .isLength({ min: 5 })
+    .isAlphanumeric(),
+  body("confirmPassword").custom((value, { req }) => {
+    //custom comparator
+    if (value != req.body.password) {
+      throw new Error("Passwords and Confirm Password do NOT match!");
+    }
+    return true;
+  }),
+  authController.postSignup
+);
 
-router.post('/logout', authController.postLogout);
+router.post("/logout", authController.postLogout);
+
+router.get("/reset", authController.getReset);
+
+router.post("/reset", authController.postReset);
+
+router.get("/reset/:token", authController.getNewPassword);
+
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
